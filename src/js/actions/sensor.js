359ca27997cec252ad6dbae3ca4bd2b138c9c052
nbelleme <nbelleme@e-biz.fr>
2018-03-15 09:10:58 -0400

    change sensor endpoints

--- src/js/actions/sensor.js
+++ src/js/actions/sensor.js
@@ -23,7 +23,7 @@ export const LOAD_ALL_SENSORS_FAILURE = "LOAD_ALL_SENSORS_FAILURE";
 const callLoadAllSensors = () => ({
   [CALL_API]: {
     types: [LOAD_ALL_SENSORS_REQUEST, LOAD_ALL_SENSORS_SUCCESS, LOAD_ALL_SENSORS_FAILURE],
-    endpoint: `sensor/all`,
+    endpoint: `sensors/all`,
     api: API_SENSOR,
     method: 'GET',
   }
@@ -45,7 +45,7 @@ export const ACTIVATE_SENSOR_FAILURE = "ACTIVATE_SENSOR_FAILURE";
 const callActivateSensor = (label, activate) => ({
   [CALL_API]: {
     types: [ACTIVATE_SENSOR_REQUEST, ACTIVATE_SENSOR_SUCCESS, ACTIVATE_SENSOR_FAILURE],
-    endpoint: `sensor/${label}/activate/${activate}`,
+    endpoint: `sensors/${label}/activate/${activate}`,
     api: API_SENSOR,
     method: 'PUT',
   }
@@ -67,7 +67,7 @@ export const DELETE_SENSOR_FAILURE = "DELETE_SENSOR_FAILURE";
 const callDeleteSensor = (label) => ({
   [CALL_API]: {
     types: [DELETE_SENSOR_REQUEST, DELETE_SENSOR_SUCCESS, DELETE_SENSOR_FAILURE],
-    endpoint: `sensor/${label}`,
+    endpoint: `sensors/${label}`,
     api: API_SENSOR,
     method: 'DELETE',
   }
@@ -89,7 +89,7 @@ export const CREATE_SENSOR_FAILURE = "CREATE_SENSOR_FAILURE";
 const callCreateSensor = (sensor) => ({
   [CALL_API]: {
     types: [CREATE_SENSOR_REQUEST, CREATE_SENSOR_SUCCESS, CREATE_SENSOR_FAILURE],
-    endpoint: `sensor`,
+    endpoint: `sensors`,
     api: API_SENSOR,
     method: 'POST',
     body: sensor
@@ -112,7 +112,7 @@ export const EDIT_SENSOR_FAILURE = "EDIT_SENSOR_FAILURE";
 const callEditSensor = (label, sensor) => ({
   [CALL_API]: {
     types: [EDIT_SENSOR_REQUEST, EDIT_SENSOR_SUCCESS, EDIT_SENSOR_FAILURE],
-    endpoint: `sensor/${label}`,
+    endpoint: `sensors/${label}`,
     api: API_SENSOR,
     method: 'PUT',
     body: sensor
